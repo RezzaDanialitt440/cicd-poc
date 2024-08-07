280d88f700c7bd495e021871a0524a8d479d1900
Rezza Danial <rezzadanialcs@gmail.com>
2024-08-07 10:47:49 +0800

    update code

--- compare.sh
+++ compare.sh
@@ -4,7 +4,7 @@ commit1=845fcfc
 commit2=270603c
 
 # Run the commands
-dirs=$(git diff --name-only $commit1 $commit2 | grep -v '^\.' | cut -d'/' -f1 | sort | uniq)
+dirs=$(echo "" | grep -v '^\.' |  grep -v '^script/' | cut -d'/' -f1 | sort | uniq)
 dirs=$(echo $dirs | tr '\n' ' ')
 
 # Print the result
